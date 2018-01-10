387ee1466f31eb1d115cedd8de6928f59b109259
GeraldineE <geraldine.e.cortes@gmail.com>
2018-01-10 04:15:17 -0500

    Estrellas visibles superpuestas sobre las estrellas totales. Hay que invertir el eje

--- DiagramaHr/diagramaHR.py
+++ DiagramaHr/diagramaHR.py
@@ -1,4 +1,4 @@
-#Diagrama HR para relacionar las propiedades de las estrellas
+#Diagrama HR(Hertzsprung-Russell) para relacionar las propiedades de las estrellas
 #Temperatura-Magnitud Abs
 
 from numpy  import *
@@ -6,16 +6,21 @@ from matplotlib.pyplot import *
 
 datos=genfromtxt("hygdata_v3.csv", delimiter=",") #Cargamos los datos
 
+magnitud_aparente=datos[1:,13]
 mag_abs= datos[1:,14]
 color_index= datos[1:,16]
 
+filtro=magnitud_aparente<5
+
 figure()
-plot(color_index,mag_abs,'o',color='blue',markersize=-1)
+plot(color_index,mag_abs,'o',color='red',markersize=0.5)
+plot(color_index[filtro],mag_abs[filtro],'o',color='yellow',markersize=1)
+
 savefig("diagramaHR.png")
 xlabel("$BV")
-xlim((-0.5,3.0))
+xlim(-0.5,3.0)
 ylabel("$Magnitud")
-ylim((20,-10))
+ylim(20,-10)
 
 
 
